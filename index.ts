b63c9a5920f186a8e602c1383267590681c99586
Leonid Pospelov <pospelovlm@yandex.ru>
2022-05-26 08:59:17 +0300

    It works

--- index.ts
+++ index.ts
@@ -145,6 +145,63 @@ const getCompanyName = (jobInfo: JobInfo) => {
   return jobInfo.div.div[1].div[0].h3.a;
 };
 
+interface JobInfoParsed {
+  company: string,
+  position: string,
+  hiringContact: string
+}
+
+const generateLetter = (letterTemplate: string, infoParsed: JobInfoParsed) => {
+  const cfg: Record<string, string> = {
+    "coder": infoParsed.position,
+    "your company": infoParsed.company,
+    "hiring manager": infoParsed.hiringContact
+  };
+
+  let contents = letterTemplate;
+
+  for (const key in cfg) {
+    for (; ;) {
+      let res = contents.replace(key, cfg[key] as string);
+      if (res === contents) {
+        break;
+      }
+      else {
+        contents = res;
+      }
+    }
+  }
+
+  return contents;
+};
+
+const getJobInfoParsed = async (chromeConnect: ChromeConnectSuccess) => {
+  const result = await chromeConnect.client.Runtime.evaluate({
+    expression: `document.getElementsByClassName("styles_jobInfo__FTdPp")[0].outerHTML` // TODO: Unhardcode. Looks unreliable
+  });
+  const evaluateResult = result.result.value;
+
+  const res = (new xml.XMLParser()).parse(evaluateResult);
+
+  const position = getPosition(res);
+  const company = getCompanyName(res);
+
+  const result1 = await chromeConnect.client.Runtime.evaluate({
+    expression: `document.documentElement.innerHTML`
+  });
+  const evaluateResult1 = result1.result.value as string;
+
+  let tmp = evaluateResult1.slice(
+    23 + evaluateResult1.indexOf("Your hiring contact is"));
+  tmp = tmp.slice(0, tmp.indexOf('<'));
+
+  // No name so just calling this person "hiring manager"
+  if (evaluateResult1.indexOf("Your hiring contact is") === -1) {
+    tmp = "hiring manager";
+  }
+  return { position, company, hiringContact: tmp };
+};
+
 const main = async () => {
   if (`${nutTemplateMatcher}` === "this cond is never true") {
     console.log(nutTemplateMatcher);
@@ -178,6 +235,10 @@ const main = async () => {
 
   let wasOnAngel = null;
 
+  let totalApplications = 0;
+
+  let applications = new Array<JobInfoParsed>();
+
   for (; ;) {
     // DO NOT REMOVE THIS LINE
     // It pervents event loop issue
@@ -198,8 +259,7 @@ const main = async () => {
     }
 
     if (isOnAngel) {
-      // 1. найти кнопку Apply и кликнуть
-      // 2. написать в консоль
+      await findAndClick("assets/angel-co-skip-ass.png");
 
       const res1 = await findAndClick("assets/angel-co-apply.png");
       if (res1.success) {
@@ -210,36 +270,127 @@ const main = async () => {
       const res = await findAndClick("assets/angel-co-write-a-note.png");
 
       if (!res.success) {
+
         console.log("Scrolling down");
         await nut.mouse.scrollDown(300);
       }
       if (res.success) {
 
-        const result = await chromeConnect.client.Runtime.evaluate({
-          expression: `document.getElementsByClassName("styles_jobInfo__FTdPp")[0].outerHTML` // TODO: Unhardcode class name. Looks unreliable
-        });
-        const evaluateResult = result.result.value;
+        const infoFull = await getJobInfoParsed(chromeConnect);
 
-        const parser = new xml.XMLParser();
+        console.log(infoFull);
 
-        const res = parser.parse(evaluateResult);
-        // console.log(JSON.stringify(res, null, 2));
+        let newPosition = "";
+        let newHiringContact = "";
 
-        console.log(getPosition(res));
-        console.log(getCompanyName(res));
+        const html = (await chromeConnect.client.Runtime.evaluate({
+          expression: `document.documentElement.innerHTML`
+        })).result.value as string;
 
-        /*console.log("Copy-pasting cover letter");
-        const letterTemplate = fs.readFileSync("./Cover letter.md", "utf-8");
-        await nut.clipboard.copy(letterTemplate);
-        await nut.keyboard.pressKey(nut.Key.LeftControl, nut.Key.V);
-        await nut.keyboard.releaseKey(nut.Key.LeftControl, nut.Key.V);*/
+        if (infoFull.position.toLowerCase().includes("ethereum")
+          || infoFull.position.toLowerCase().includes("contract")
+          || infoFull.position.toLowerCase().includes("smart")
+          || infoFull.position.toLowerCase().includes("relations")
+          || infoFull.position.toLowerCase().includes("blockchain")
+          || infoFull.position.toLowerCase().includes("ai")
+          || infoFull.position.toLowerCase().includes("devops")
+          || infoFull.position.includes("Only")) {
+          console.log("Skipping " + infoFull.position);
+          await nut.keyboard.pressKey(nut.Key.Escape);
+          await nut.keyboard.releaseKey(nut.Key.Escape);
+          await nut.mouse.scrollDown(500);
+        }
+        else if (applications.find(x => x.company === infoFull.company)) {
+          console.log("Skipping company (Already applied!)");
+          await nut.keyboard.pressKey(nut.Key.Escape);
+          await nut.keyboard.releaseKey(nut.Key.Escape);
+          await nut.mouse.scrollDown(500);
+        }
+        else if (html.indexOf("This job does not support the locations or timezones for remote work on your profile.") !== -1) {
+          console.log("Skipping company (Bad timezone!)");
+          await nut.keyboard.pressKey(nut.Key.Escape);
+          await nut.keyboard.releaseKey(nut.Key.Escape);
+          await nut.mouse.scrollDown(500);
+        }
+        else if (html.indexOf("does not offer visa sponsorship and requires all remote workers to be in-country.") !== -1) {
+          console.log("Skipping company (Does not offer visa sponsorship!)");
+          await nut.keyboard.pressKey(nut.Key.Escape);
+          await nut.keyboard.releaseKey(nut.Key.Escape);
+          await nut.mouse.scrollDown(500);
+        }
+        else if (html.indexOf("is not accepting applications from your current location due to timezone or relocation constraints") !== -1) {
+          console.log("Skipping company (Hates my timezone!)");
+          await nut.keyboard.pressKey(nut.Key.Escape);
+          await nut.keyboard.releaseKey(nut.Key.Escape);
+          await nut.mouse.scrollDown(500);
+        }
+        else {
+          newPosition = "Software Engineer";
+          if (infoFull.position.toLowerCase().includes("full") && infoFull.position.toLowerCase().includes("stack")) {
+            newPosition = "Full-Stack Developer";
+          }
+          else if (infoFull.position.toLowerCase().includes("back")) {
+            newPosition = "Backend Developer";
+          }
+          else if (infoFull.position.toLowerCase().includes("front")) {
+            newPosition = "Frontend Developer";
+          }
+
+          if (infoFull.position.toLowerCase().includes("senior")) {
+            newPosition = "Senior " + newPosition;
+          }
+        }
+
+        newHiringContact = infoFull.hiringContact;
+        // `Andrew O` => `Andrew` (real case)
+        if (`${infoFull.hiringContact.split(' ')[1]}`.length === 1) {
+          newHiringContact = '' + infoFull.hiringContact.split(' ')[0];
+        }
+
+        // if skipped
+        if (!newPosition) {
+          await nut.keyboard.pressKey(nut.Key.Escape);
+          await nut.keyboard.releaseKey(nut.Key.Escape);
+          await nut.mouse.scrollDown(500);
+        }
+
+        // if not skipped
+        if (newPosition) {
+
+          infoFull.position = newPosition;
+          infoFull.hiringContact = newHiringContact;
+
+          console.log({ newPosition, newHiringContact });
+
+
+          console.log("Copy-pasting cover letter");
+          const letterTemplate = fs.readFileSync("./Cover letter.md", "utf-8");
+          const letter = generateLetter(letterTemplate, infoFull);
+          console.log(letter);
+          await nut.clipboard.copy(letter);
+          await nut.keyboard.pressKey(nut.Key.LeftControl, nut.Key.V);
+          await nut.keyboard.releaseKey(nut.Key.LeftControl, nut.Key.V);
+
+          console.log("Waiting 500ms for no reason");
+          await new Promise(r => setTimeout(r, 500));
+
+          // Maybe we already wrote the letter and want to submit
+          const clickSend = await findAndClick("assets/angel-co-send-application.png");
+          await nut.keyboard.pressKey(nut.Key.Escape);
+          await nut.keyboard.releaseKey(nut.Key.Escape);
+
+          applications.push(infoFull);
+          totalApplications++;
+          console.log({ totalApplications });
+          await nut.mouse.scrollDown(400);
+          if (totalApplications == 10) {
+            console.log("Done");
+            process.exit(0);
+          }
+        }
       }
     }
   }
 }
 
 main();
-
-// hiring manager
-// coder
-// your company
